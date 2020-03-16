abad6a16b36e05ea0668c09223296f71f7bd8790
alejandrodk <ottoabarriosp@hotmail.com>
2020-03-16 16:42:10 -0300

    Actualizacion de API REST

--- Api/controller.js
+++ Api/controller.js
@@ -1,3 +1,6 @@
+const db = require("../database/models");
+const sequelize = db.sequelize;
+
 const sql = require("mssql");
 const config = {
             user: "sa",
@@ -7,25 +10,65 @@ const config = {
         };
 
 module.exports = {
-    credito : (req, res) => {
+    general : (req, res) => {
+		// enviar todos los comprobantes
+		let cuenta = req.params.cliente;
 
-        sql.connect(config)
-        .then(() => {
-            return sql.query`select STMPDH_ARTCOD,STMPDH_DESCRP,USR_STMPDH_TITULO from stmpdh `
-        })
-        .then(result => {
-
-            let output = result.recordset.map(item => {
-                return {
-                    articulo : item.STMPDH_ARTCOD,
-                    descripcion: item.STMPDH_DESCRP,
-                    modelos: item.USR_STMPDH_TITULO
-                }
+        db.comprobantes.findAll({ where : { cliente_num : cuenta } })
+		.then(result => {
+                return res.send(result)
             })
-            res.json(output);
-        })
-        .catch(error => {
-            res.send(error);
-        })
-    }
+	},
+	numero : (req, res) => {
+		// enviar informacion de un comprobante por numero
+	},
+	tipo : (req, res) => {
+		// enviar todos los comprobantes del mismo tipo
+		let {tipo,cliente} = req.params;
+		let tabla = '';
+
+		if(tipo == 'credito') tabla = 'dbo.ncXdto';
+		if(tipo == 'debito') tabla = 'dbo.ncXdto';
+		if(tipo == 'factura') tabla = 'dbo.ncXdto';
+		
+            sql.connect(config, function(err) {
+              if (err) console.log(err);
+
+              var request = new sql.Request();
+
+              request.query("select top 10 nrocta,nrofor,factura,monto,codfor,nrocae from "+ tabla +" WHERE nrocta = 00" + cliente, function(err, result) {
+                if (err) res.send(err);
+                if(result.recordset != undefined){
+                	return res.send(result.recordset);
+                }
+              });
+            });
+	},
+	detalle : (req, res) => {
+		let {cliente, numero} = req.params;
+		let 
+			datosCliente = db.clientes.findOne({
+				where : { numero : cliente},
+				attributes : {exclude : ['telefono','correo','precio_especial','transporte']},
+			})
+			comprobantes = db.comprobantes.findAll( { where : { numero : numero }, attributes : {exclude : ['cliente_num']} })
+			articulos = sequelize.query(`
+			SELECT comp_articulo.articulo_id, comp_articulo.cantidad, comp_articulo.precio, comp_articulo.despacho, articulos.descripcion, articulos.modelos
+			FROM comp_articulo
+			JOIN articulos ON comp_articulo.articulo_id = articulos.codigo
+			WHERE numero = ${numero}`)
+		
+			Promise.all([datosCliente,comprobantes,articulos])
+			//.then(result => res.send(result[2]))
+			.then(result => {
+				return res.json({
+					status_code : res.statusCode,
+					response : {
+						cliente : result[0],
+						comprobante : result[1],
+						articulos : result[2]
+					}
+				})
+			}) .catch( error => res.send(error))
+	}
 }
--- Api/routes.js
+++ Api/routes.js
@@ -2,6 +2,15 @@ const express = require("express");
 const router = express.Router();
 const apiController = require('./controller')
 
-router.get('/comprobantes/credito', apiController.credito)
+// trae todos los comprobantes de un cliente
+router.get('/comprobantes/cliente/:cliente', apiController.general)
+// trae un comprobante especifico de un cliente
+router.get('/comprobantes/cliente/:cliente/:numero', apiController.detalle)
+// trae un comprobante especifico
+router.get('/comprobantes/numero/:numero', apiController.numero)
+// trae todos los comprobantes de un mismo tipo
+router.get('/comprobantes/tipo/:tipo', apiController.tipo)
+// trae todos los comprobantes de un mismo tipo y cliente
+router.get('/comprobantes/tipo/:tipo/:cliente', apiController.tipo)
 
 module.exports = router;
