a6a6819c400024325b0c5a5c5552e000eb406696
alejandrodk <ottoabarriosp@hotmail.com>
2020-03-19 12:06:49 -0300

    Antiguedad pedido cliente

---
+++ public/js/viajantes/dateDif.js
@@ -0,0 +1,33 @@
+let salidas = document.querySelectorAll('.fechaSalida')
+let plazos = document.querySelectorAll('.plazoSalida')
+
+
+function getDate(){
+    let fecha = new Date();
+
+    let dd = fecha.getDate();
+    let mm = fecha.getMonth() + 1;
+    let yyyy = fecha.getFullYear();
+
+    let fechaAct = `${yyyy}-${mm}-${dd}`;
+    return fechaAct;
+}
+function formatDate(fecha){
+    let arr = fecha.split('/');
+    let fechNueva = `${arr[2]}-${arr[1]}-${arr[0]}`
+    return fechNueva;
+}
+for (let i = 0; i < salidas.length; i++){
+
+    if(salidas[i].innerHTML != ''){
+
+        let fechaAct = moment(getDate());
+        let fechaComp = moment(formatDate(salidas[i].innerHTML));
+        
+        let plazo = fechaAct.diff(fechaComp, 'days');
+        
+        plazos[i].innerHTML = plazo + ' días';
+    }
+    
+
+}
