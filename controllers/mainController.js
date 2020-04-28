dd90c3cabafbf51b5558fc6bc94d81a2d2361779
alejandrodk <ottoabarriosp@hotmail.com>
2020-04-28 20:51:22 -0300

    Prueba email home

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -9,34 +9,19 @@ const controller = {
 		res.render("main/index", { title_login : title_login });
 	  },
 	formulario : (req, res) => {
-		// create reusable transport method (opens pool of SMTP connections)
-		var smtpTransport = nodemailer.createTransport("SMTP",{
-		    service: "Gmail",
-		    auth: {
-		        user: "dkone2794@gmail.com",
-		        pass: "dasker2110"
-		    }
+		let transporter = nodemailer.createTransport({
+			sendmail: true,
+			newline: 'unix',
+			path: '/usr/sbin/sendmail'
 		});
-
-		// setup e-mail data with unicode symbols
-		var mailOptions = {
-		    from: "Fred Foo ✔ <foo@blurdybloop.com>", // sender address
-		    to: "ottoabarriosp@hotmail.com", // list of receivers
-		    subject: "Hello ✔", // Subject line
-		    text: "Hello world ✔", // plaintext body
-		    html: "<b>Hello world ✔</b>" // html body
-		}
-
-		// send mail with defined transport object
-		smtpTransport.sendMail(mailOptions, function(error, response){
-		    if(error){
-		        console.log(error);
-		    }else{
-		        console.log("Message sent: " + response.message);
-		    }
-		
-		    // if you don't want to use this transport object anymore, uncomment following line
-		    smtpTransport.close(); // shut down the connection pool, no more messages
+		transporter.sendMail({
+			from: 'info@webapp.altamiragroup.com.ar',
+			to: 'ottoabarriosp@hotmail.com',
+			subject: 'Prueba',
+			text: 'Mensaje de prueba'
+		}, (err, info) => {
+			console.log(info.envelope);
+			console.log(info.messageId);
 		});
 	},
 	login: (req, res) => {
