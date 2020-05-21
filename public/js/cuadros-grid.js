0163011f143a3a3ccf7ea1f247de7813aa193134
alejandrodk <ottoabarriosp@hotmail.com>
2020-05-21 14:06:38 -0300

    ajuste grilla articulos

---
+++ public/js/cuadros-grid.js
@@ -0,0 +1,23 @@
+let css = document.querySelector('#cuadros-grid')
+let th2 = document.querySelector('#th-2')
+let th3 = document.querySelector('#th-3')
+let th4 = document.querySelector('#th-4')
+
+function ajustarGrilla(){
+    let grilla = sessionStorage.getItem('grid')
+    css.setAttribute('href', grilla)
+}
+ajustarGrilla()
+
+th2.addEventListener('click',function(){
+    sessionStorage.setItem('grid','/css/main/catalogo/cuadros-grid-2.css')
+    ajustarGrilla()
+})
+th3.addEventListener('click',function(){
+    sessionStorage.setItem('grid','/css/main/catalogo/cuadros-grid-3.css')
+    ajustarGrilla()
+})
+th4.addEventListener('click',function(){
+    sessionStorage.setItem('grid',' ')
+    ajustarGrilla()
+})
