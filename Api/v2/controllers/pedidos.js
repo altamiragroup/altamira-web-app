61d15f1c5f1b19c637d70afc21ff6844f5c40213
alejandrodk <ottoabarriosp@hotmail.com>
2020-06-03 16:46:17 -0300

    Api V2 Pedidos

--- Api/v2/controllers/pedidos.js
+++ Api/v2/controllers/pedidos.js
@@ -1,5 +1,74 @@
+const db = require('../../../database/models');
+
 module.exports = {
-    pedidos : (req, res) => {
-        
+
+    pedidos : async (req, res) => {
+
+        let articulos = req.query.articulos;
+        let cuenta = req.query.cuenta;
+
+        try {
+            let pedidos = await db.pedidos.findAll({
+                where : cuenta ? { cliente_id : cuenta } : { },
+                logging: false,
+                include : articulos ? [{ model: db.articulos, as: 'articulos' }] : ''
+            });
+            return res
+            .status(200)
+            .json({ pedidos })
+        }
+        catch(err){
+            return res
+            .status(500)
+            .json({
+                message : 'Error',
+                err
+            })   
+        }
+    },
+    detalle : async (req, res) => {
+        let id = req.params.id;
+        let articulos = req.query.articulos;
+
+        try {
+            let pedidos = await db.pedidos.findAll({
+                where : { id },
+                include : articulos ? [{ model: db.articulos, as: 'articulos' }] : '',
+                logging: false
+            })
+            return res
+            .status(200)
+            .json({ pedidos })
+        }
+        catch(err){
+            return res
+            .status(500)
+            .json({
+                message : 'Error',
+                err
+            })   
+        }
+    },
+    nuevo : (req, res) => {
+
+    },
+    eliminar : async (req, res) => {
+        let id = req.params.id;
+
+        try {
+            let pedido = await db.pedidos.destroy({
+                where : { id },
+                logging: false
+            })
+            return res.status(204)
+        }
+        catch(err){
+            return res
+            .status(500)
+            .json({
+                message : 'Error',
+                err
+            })   
+        }
     }
 }
--- Api/v2/routes/pedidos.js
+++ Api/v2/routes/pedidos.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/pedidos')
 
 router.get('/', controller.pedidos)
+router.get('/:id', controller.detalle)
 
 module.exports = router
