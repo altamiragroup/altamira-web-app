e8db8def9e33fcf1f804cd9f012ecbe1cf2a4a1a
alejandrodk <ottoabarriosp@hotmail.com>
2020-08-25 09:52:22 -0300

    Envio de prueba email

---
+++ tasks/envio-prueba.js
@@ -0,0 +1,38 @@
+const mailer = require('../helpers/mailHelp');
+const cron = require('node-cron');
+
+async function enviarListaPrecios(){
+    try {
+        const clientes = [{
+            razon_social: 'Altamira Group',
+            correo: 'info@altamiragroup.com.ar'
+        },{
+            razon_social: 'Altamira Group',
+            correo: 'publicidad@altamiragroup.com.ar'
+        }]
+        async function enviarEmail(cliente){
+            const { razon_social, correo } = cliente;
+            await mailer.listaPrecios(razon_social, correo);
+        }
+
+        for(item of clientes){
+            if(item.correo != ''){
+                await enviarEmail(item)
+            }
+        }
+    }
+    catch(e){
+        console.error(e)
+    }
+}
+// se envia al correr el proceso
+ enviarListaPrecios()
+// se envia cada viernes
+let task = cron.schedule('0 16 * * 5', () =>  {
+  enviarListaPrecios()
+},{
+   scheduled: true,
+   timezone: "America/Argentina/Buenos_Aires"
+});
+ 
+task.start();
