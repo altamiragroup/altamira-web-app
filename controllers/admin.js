6e58e0f0ceedcd17a53ec85952e4faaf7b4b1095
altamiragroup <publicidad@altamiragroup.com.ar>
2020-11-02 08:10:10 -0300

    actualizacion order by lista de pedidos

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,7 @@ module.exports = {
                 attributes : ['id','cliente_id','fecha'],
                 include : [{ model: db.clientes, as: 'cliente', attributes: ['razon_social']}],
                 group : ['pedidos.id','pedidos.cliente_id','pedidos.fecha','cliente.razon_social','cliente.numero'],
-                order : [['id','fecha','DESC']],
+                order : [['id','DESC']],
                 limit : 10,
                 logging: false
             });
