a5e097b715488fc1f0136fd164b31ebd630a57f4
alejandrodk <ottoabarriosp@hotmail.com>
2021-04-30 10:38:08 -0300

    Update cart schema

--- database/mongo/schemas/schemas.js
+++ database/mongo/schemas/schemas.js
@@ -1,17 +1,30 @@
-'use strict';
-const mongoose = require('../config/mongoose');
+"use strict";
+const mongoose = require("../config/mongoose");
 const Schema = mongoose.Schema;
 
 const schemas = {
-    cartSchema: new Schema({
-        cliente : { type: Number, unique: true },
-        actualizado : String,
-        articulos : [],
-        values : {
-            descuento : Number,
-            total : Number
-        }
-    })
-}
+  cartSchema: new Schema({
+    cliente: { type: Number, unique: true },
+    actualizado: String,
+    articulos: {
+      type: [
+        {
+          codigo: String,
+          linea: Number,
+          cantidad: Number,
+          min_vta: Number,
+          stock: Number,
+          precio: Number,
+          descripcion: String,
+        },
+      ],
+      default: [],
+    },
+    values: {
+      descuento: Number,
+      total: Number,
+    },
+  }),
+};
 
 module.exports = schemas;
