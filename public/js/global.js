2a7aeb6bb36cb76f40c33382f8aa327de62d6caa
altamira <publicidad@altamiragroup.com.ar>
2026-03-16 08:30:57 -0300

    CORRIGE MENU TOGGLE

--- public/js/global.js
+++ public/js/global.js
@@ -1,6 +1,7 @@
 let boton = document.querySelector('.menu-toggle');
-let menu = document.querySelector('.page-header .menu');
+let menu = document.querySelector('.menu');
 
 boton.addEventListener('click', function(){
-  menu.classList.toggle('active')
-})
+  menu.classList.toggle('active');
+  boton.classList.toggle('on');
+});
